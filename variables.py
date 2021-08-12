16f60d85ce6f5cf119890d7bef88a5475bff005f
Rima Mondal <rima23091996@gmail.com>
2021-08-12 18:00:57 +0000

    variables

--- variables.py
+++ variables.py
@@ -1,2 +1,13 @@
 x=9
+y=3
 print(x+10)
+name='youtube'
+print(name+" "+'mondal')
+print(name[0])
+print(name[-1])
+print(name[1:3])
+
+print(name[:1])
+print(name[3:10])
+print('my' + name[3:6])
+print(len(name))
