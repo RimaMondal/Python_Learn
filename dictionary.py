c7243816b583d4923c494ed42eb84b4a158f6bb7
Rima Mondal <rima23091996@gmail.com>
2021-08-15 18:08:33 +0000

    datatypes_dictionary

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+d={'navin':'samsung','rima':'iphone'}
+print(d)#{'navin': 'samsung', 'rima': 'iphone'}
+print(d.keys()) #dict_keys(['navin', 'rima'])
+print(d.values()) #dict_values(['samsung', 'iphone'])
+print(d['rima'])
--- tuple.py
+++ tuple.py
@@ -6,3 +6,19 @@ s={10,20,30,40}
 print(s)
 s.add(60)
 print(s)
+
+#address of a variable
+name='rima'
+print(id(name))
+
+a=2.3
+
+print(type(a))
+b=int(a)
+print(type(b))
+k=6
+p=2
+q=complex(k,p) #complex
+print(q)
+#print(list(range(10)))
+print(list(range(2,10,2)))
